fa75c9a9a77228794b670b868af55d4efc02fa04
Kurzanovv <ivan.sokolovsky.work@yandex.ru>
2024-03-03 21:22:10 +0500

    Загрузка файлов на github [#243562 #35662]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ivan Kurzanov");
+        System.out.println("05.07.2002");
+
+    }
+}
